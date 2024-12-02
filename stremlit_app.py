9055dd99337f375e7375de0a5207dfd1c983cfcc
Jaydev Atodariya <36396527+atodariyajh@users.noreply.github.com>
2024-12-02 11:31:12 +0530

    Update stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -11,5 +11,7 @@ from snowflake.snowpark.functions import col
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
+session = get_Active_session()
+
 cnx = st.connection("snowflake")
 session = cnx.session()
