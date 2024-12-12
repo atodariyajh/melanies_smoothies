547dcffdef9a811837ebf4b0361c3c3ee92ab24a
Jaydev Atodariya <36396527+atodariyajh@users.noreply.github.com>
2024-12-12 11:15:08 +0530

    Update stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -1,42 +1,56 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col,when_matched
+from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
-st.title(":cup_with_straw: Customize Your smoothiee:cup_with_straw:")
+st.title(":cup_with_straw: Customize Your Smoothie  :cup_with_straw:")
 st.write(
-    """Name on Smoothie:"""
+    """Choose the fruits you want in your custom smoothie"""
 )
 
-st.write(
-    """The name on your smoothie will be:"""
-)
-cnx = st.connection("snowflake")
+name_on_order = st.text_input("Name of Smoothie")
+st.write("The Name on your smoothie will be :", name_on_order)
+
+from snowflake.snowpark.functions import col
+
+cnx=st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
-editable_df = st.data_editor(my_dataframe)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col ('SEARCH_ON'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 
-submitted = st.button('Submit')
+#convert the snowpark dataframe to a pandas dataframe so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
 
-if my_dataframe:
-    editable_df=st.experimental_data_editor(my_dataframe)
-    submitted = st.button('Submit')
-    if submitted:
+ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe,max_selections=5)
+
+if ingredients_list:
+    ingredients_string=''
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen+' '
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
+        #st.subheader(fruit_chosen + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+                values ('""" + ingredients_string + """','""" + name_on_order + """')"""
     
-        #st.success('Someone clicked the button', icon = '👍')
+    st.write(my_insert_stmt)
+    #st.stop()
+    time_to_insert=st.button('Submit Order')
 
-        og_dataset = session.table("smoothies.public.orders")
-        edited_dataset = session.create_dataframe(editable_df)
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
 
-        try:
-            og_dataset.merge(edited_dataset
-                         , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
-                         , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
-                        )
-            st.success("Order(s) updated!", icon = '👍')
-        except:
-            st.write('Something went wrong.')
 
-else:
-    st.success('There are no pending orders right now', icon = '👍')
 
