615dc1d76b4f3aae1614c96d909f76612d59c97b
Jaydev Atodariya <36396527+atodariyajh@users.noreply.github.com>
2024-12-12 18:02:27 +0530

    Update stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -37,8 +37,8 @@ if ingredients_list:
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         #st.subheader(fruit_chosen + 'Nutrition Information')
-        #smoothiefroot_response = requests.get("https://my.smoothieFroot.com/api/fruit/" + fruit_chosen)
-        smoothiefroot_response = requests.get("https://my.smoothieFroot.com/api/fruit/" + search_on)
+        smoothiefroot_response = requests.get("https://my.smoothieFroot.com/api/fruit/" + fruit_chosen)
+        #smoothiefroot_response = requests.get("https://my.smoothieFroot.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
